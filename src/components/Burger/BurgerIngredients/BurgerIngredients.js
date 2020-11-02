9649d46e2361bd6e293e39d0749a6c66758286fd
mysterymethod <chandra.pranay@yahoo.in>
2020-11-02 13:34:26 +0530

    Adding prop types v-11

---
+++ src/components/Burger/BurgerIngredients/BurgerIngredients.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import classes from './BurgerIngredients.Module.css';
+
+const burgerIngredients (props) => {
+    let ingredients = null;
+
+    switch (props.type) {
+        case 'BreadBottom':
+            ingredients = <div className={}></div>
+            
+            break;
+    
+        default:
+            break;
+    }
+
+}
