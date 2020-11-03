361f6013b74e5bd0b8f8b3428973c682c3bb2562
mysterymethod <chandra.pranay@yahoo.in>
2020-11-03 17:35:56 +0530

    modal close on click

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,21 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Aux from '../../hoc/Auxiliary';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import classes from './Layout.module.css';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => (
-    <Aux>
-        <div>
-            <Toolbar />
-            <SideDrawer />
-        </div>
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
-    
-);
 
-export default layout;
+class Layout extends Component {
+
+    state = {
+        showSideDrawer: true,
+    }
+
+    SideDrawerCloseHandler = () => {
+        this.setState({showSideDrawer: false});
+    }
+
+
+    render () {
+        return (
+            <Aux>
+                <div>
+                    <Toolbar />
+                    <SideDrawer 
+                        show={this.state.showSideDrawer} 
+                        click={this.SideDrawerCloseHandler}  
+                    />
+                </div>
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux> 
+        );
+    }
+}
+
+
+export default Layout;
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -3,13 +3,25 @@ import React from 'react';
 import classes from './SideDrawer.module.css';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Aux from '../../../hoc/Auxiliary';
 
 const sideDrawer = (props) => {
+
+    let attachedClasses = [classes.SideDrawer, classes.Close];
+    if (props.show) {
+        attachedClasses = [classes.SideDrawer, classes.Open];
+    }
+
     return (
-        <div className={classes.SideDrawer}>
-            <Logo height='11%' />
-            <NavigationItems />
-        </div>
+        <Aux>
+            <Backdrop show={props.show} clicked={props.click} />
+            <div className={attachedClasses.join(' ')}>
+                <Logo height='11%' />
+                <NavigationItems />
+            </div>
+        </Aux>
+        
     );
 }
 
