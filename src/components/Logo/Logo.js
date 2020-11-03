76de0c24ea4772b70ca49337887352d9ff30db99
mysterymethod <chandra.pranay@yahoo.in>
2020-11-03 13:18:21 +0530

    Added logo in toolbar

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import BurgerLogo from '../../assets/images/burger-logo.png';
+import classes from './Logo.module.css';
+
+const logo = (props) => {
+    return (
+        <div className={classes.Logo}>
+            <img src={BurgerLogo} alt="BurgerLogo" />
+        </div>
+    );
+}
+
+export default logo;
