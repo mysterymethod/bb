889901f5768155098a363d4e6769ade93ee38b49
mysterymethod <chandra.pranay@yahoo.in>
2020-11-03 16:17:51 +0530

    responsive logo in sidedrawer

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -7,7 +7,7 @@ import NavigationItems from '../NavigationItems/NavigationItems';
 const sideDrawer = (props) => {
     return (
         <div className={classes.SideDrawer}>
-            <Logo />
+            <Logo height='11%' />
             <NavigationItems />
         </div>
     );
