6850a0328afc6a49184c82247c048991b227281b
mysterymethod <chandra.pranay@yahoo.in>
2020-11-03 16:08:04 +0530

    Added sidedrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,11 +3,13 @@ import React from 'react';
 import Aux from '../../hoc/Auxiliary';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import classes from './Layout.module.css';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 const layout = (props) => (
     <Aux>
         <div>
             <Toolbar />
+            <SideDrawer />
         </div>
         <main className={classes.Content}>
             {props.children}
