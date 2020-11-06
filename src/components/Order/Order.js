36f567ffc1bd0bb1a0714d8a543debc90fbb1e1e
mysterymethod <chandra.pranay@yahoo.in>
2020-11-06 15:35:26 +0530

    Added orders page

---
+++ src/components/Order/Order.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import classes from './Order.module.css';
+
+const order = (props) => {
+    return (
+        <div className={classes.Order}>
+            <p>Ingredients</p>
+            <p>Price</p>
+        </div>
+    );
+}
+
+export default order;
---
+++ src/containers/Orders/Orders.js
@@ -0,0 +1,17 @@
+import react, { Component } from 'react';
+
+import Order from '../../components/Order/Order';
+
+class Orders extends Component {
+    render () {
+        return (
+            <div>
+                <Order />
+                <Order />
+            </div>
+            
+        );
+    }
+}
+
+export default Orders;
